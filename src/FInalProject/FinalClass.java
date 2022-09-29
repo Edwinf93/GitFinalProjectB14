24322e943f0334066bf4a3c3c8e04b40cc1b2a56
Edwinf93 <edwinf93@gmail.com>
2022-09-28 20:11:09 -0400

    first commit

---
+++ src/FInalProject/FinalClass.java
@@ -0,0 +1,10 @@
+package FInalProject;
+
+public class FinalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is the code for mercedes");
+	}
+
+}
